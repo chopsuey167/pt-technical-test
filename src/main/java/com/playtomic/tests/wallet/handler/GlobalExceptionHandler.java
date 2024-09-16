e02051408c94d61979c41a3ab789c1f06749ddc8
Ricardo Gutierrez <ricardog.astocondor@gmail.com>
2024-09-16 20:50:39 +0200

    update handle exceptions

--- src/main/java/com/playtomic/tests/wallet/handler/GlobalExceptionHandler.java
+++ src/main/java/com/playtomic/tests/wallet/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.playtomic.tests.wallet.handler;
 
 import com.playtomic.tests.wallet.dto.ErrorDto;
+import com.playtomic.tests.wallet.exception.PaymentFailedException;
 import com.playtomic.tests.wallet.exception.WalletNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +18,15 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(WalletNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  public ResponseEntity<?> handleMessageDecodingException(WalletNotFoundException e) {
+  public ResponseEntity<?> handleWalletNotFoundException(WalletNotFoundException e) {
     return new ResponseEntity<>(buildErrorDto(e.getMessage(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(PaymentFailedException.class)
+  @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+  public ResponseEntity<?> handlePaymentFailedException(PaymentFailedException e) {
+    return new ResponseEntity<>(buildErrorDto(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY),
+        HttpStatus.UNPROCESSABLE_ENTITY);
   }
 
 }
